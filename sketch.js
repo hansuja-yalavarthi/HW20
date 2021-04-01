2aefb095ccc714be99720e1bfd64ef9708dd221e
hansuja-yalavarthi <78600451+hansuja-yalavarthi@users.noreply.github.com>
2021-04-01 15:22:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,57 +1,60 @@
-var bg;
-var tomImg1, tomImg2, tomImg3;
-var jerryImg1, jerryImg2, jerryImg3;
-var cat, mouse;
+var bg, canvas;
+var tom, tomImg1, tomImg2, tomImg3;
+var jerry, jerryImg1, jerryImg2, jerryImg3;
 
 //Fun fact - Tom is a cat, Jerry is a mouse
 
 function preload() {
-        bg = loadImage("garden.png");
-        tomImg1= loadAnimation("cat1.png", "cat2.png","cat3.png");
-        tomImg3= loadAnimation("cat4.png");
-        jerryImg1=loadAnimation("mouse1.png");
-        jerryImg2= loadAnimation("mouse2.png","mouse3.png");
-        jerryImg3=loadAnimation("mouse4.png");
+    bg = loadImage("garden.png");
+    tomImg1 = loadAnimation("cat1.png");
+    tomImg2 = loadAnimation("cat2.png", "cat3.png");
+    tomImg3 = loadAnimation("cat4.png");
+    jerryImg1 = loadAnimation("mouse1.png");
+    jerryImg2 = loadAnimation("mouse2.png", "mouse3.png");
+    jerryImg3 = loadAnimation("mouse4.png");
 }
 
-function setup(){
-    createCanvas(1000,800);
-    cat = createSprite(800, 700, 20, 20);
-    cat.addAnimation("tomCatImg", tomImg1);
-    cat.scale = 0.1;
-    mouse = createSprite(200, 700, 20, 20);
-    mouse.addAnimation("jerryMouseImg", jerryImg1);
-    mouse.scale = 0.1;
+function setup() {
+    canvas = createCanvas(1000, 800);
+
+    tom = createSprite(800, 700);
+    jerry = createSprite(200, 700);
+
+    tom.addAnimation("tomOriginal", tomImg1);
+    jerry.addAnimation("jerryOriginal", jerryImg1);
+
+    tom.scale = 0.1;
+    jerry.scale = 0.1;
 }
 
 function draw() {
 
     background(bg);
-    
-    keyPressed();
-    if (isTouching(cat, mouse)) {
-	cat.x = 800;
-	cat.changeAnimation(tomImg3);
+
+    if (tom.x - jerry.x < (tom.width - jerry.width) / 2) {
+        tom.velocityX = 0;
+        tom.addAnimation("tom3Img", tomImg3);
+        tom.x = 300;
+        tom.scale = 0.1;
+        tom.changeAnimation("tom3Img");
+        jerry.addAnimation("jerry3Img", jerryImg3);
+        jerry.scale = 0.1;
+        jerry.changeAnimation("jerry3Img");
     }
 
     drawSprites();
 }
 
-function keyPressed(){
 
-    if(keyCode === LEFT_ARROW){
-        cat.x = cat.x - 5;
+function keyPressed() {
+
+    if (keyCode === LEFT_ARROW) {
+        tom.velocityX = -5;
+        tom.addAnimation("tomRunning", tomImg2);
+        tom.changeAnimation("tomRunning");
+
+        jerry.addAnimation("jerryTeasing", jerryImg2);
+        jerry.frameDelay = 25;
+        jerry.changeAnimation("jerryTeasing");
     }
-
 }
-
-function isTouching(object1, object2) {
-    if (object1.x - object2.x < object1.width/2 + object2.width/2
-        && object2.x - object1.x < object1.width/2 + object2.width/2
-        && object1.y - object2.y < object1.height/2 + object2.height/2
-        && object2.y - object1.y < object1.height/2 + object2.height/2) {
-            result = true;
-        } else {
-            result = false;
-        }
-  }
